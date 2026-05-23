3a44d7f001ecc6743fe82d1b55bd0b7e405d70f1
huk9uri <huk9uri@gmail.com>
2026-05-23 18:45:18 +0900

    feat: `formatDateTime` 유틸 추가

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,5 +1,10 @@
+import dayjs from "dayjs"
+
 export const formatPrice = (price: number): string =>
   `${price.toLocaleString()}원`
 
 export const formatCoursePeriod = (startDate: string, endDate: string): string =>
   `${startDate} ~ ${endDate}`
+
+export const formatDateTime = (dateTime: string): string =>
+  dayjs(dateTime).format("YYYY.MM.DD HH:mm")
